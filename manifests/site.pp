609abba52103134ede6c0b99f290babb64768241
Eric Loveland <github@ericloveland.com>
2014-01-14 20:43:54 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -58,21 +58,36 @@ node default {
   include hub
   include nginx
 
+  # mine
+  include skype
+  include chrome
+  include ntfs_3g
+  include vmware_fusion
+  include spotify
+  include tmux
+  include sourcetree
+  include evernote
+  include vagrant
+
   # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
+  #if $::root_encrypted == 'no' {
+  #  fail('Please enable full disk encryption and try again')
+  #}
 
   # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
-  include nodejs::v0_10
+  #include nodejs::v0_6
+  #include nodejs::v0_8
+  include nodejs::v0_10_21
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
-  include ruby::1_9_3
-  include ruby::2_0_0
+  #include ruby::1_8_7
+  #include ruby::1_9_2
+  #include ruby::1_9_3
+  #include ruby::2_0_0
+  
+  class { 'ruby::global': 
+     version => '2.1.0',
+  }
 
   # common, useful packages
   package {
